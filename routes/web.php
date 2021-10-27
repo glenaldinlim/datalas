41cbcc882c6e040042774e4bd4c729b4b676130a
Glenaldin Lim <35095375+glenaldinlim@users.noreply.github.com>
2021-10-27 17:58:12 +0700

    Changing routes/web.php; Adding Dashboard Page on each Role

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,8 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\Backend\DashboardController;
+use App\Http\Controllers\Frontend\CommunityDashboardController;
 
 /*
 |--------------------------------------------------------------------------
@@ -12,11 +14,18 @@ use Illuminate\Support\Facades\Route;
 | contains the "web" middleware group. Now create something great!
 |
 */
+Auth::routes(['register' => false]);
 
-Route::get('/', function () {
-    return view('welcome');
+Route::group(['as' => 'front.'], function() {
+    Route::get('/', function () {
+        return view('welcome');
+    });
+    
+    Route::group(['prefix' => 'community', 'as' => 'community.', 'middleware' => ['auth', 'role:community']], function() {
+        Route::get('/dashboard', [CommunityDashboardController::class, 'index'])->name('dashboard');
+    });
 });
 
-Auth::routes();
-
-Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+Route::group(['prefix' => 'admin', 'as' => 'backend.', 'middleware' => ['auth', 'role:bod|webmaster|admin']], function() {
+    Route::get('/dashboard', [DashboardController::class, 'index'])->name('dashboard');
+});
