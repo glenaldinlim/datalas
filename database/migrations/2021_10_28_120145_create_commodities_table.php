ced059f453fb24423b024341513abbdc3893dcc8
Glenaldin Lim <35095375+glenaldinlim@users.noreply.github.com>
2021-10-28 12:14:09 +0700

    Add Commodity Model and Migration

---
+++ database/migrations/2021_10_28_120145_create_commodities_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateCommoditiesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('commodities', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('category_id');
+            $table->string('name');
+            $table->string('slug');
+            $table->boolean('is_active');
+            $table->timestamps();
+
+            $table->foreign('category_id')->references('id')->on('categories');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('commodities');
+    }
+}
