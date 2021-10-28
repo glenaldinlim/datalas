80ec206dce2dc729ebae7dd232f4773f888f1fe2
Glenaldin Lim <35095375+glenaldinlim@users.noreply.github.com>
2021-10-28 12:35:20 +0700

    Add UserLog Model and Migration

---
+++ app/Models/UserLog.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class UserLog extends Model
+{
+    use HasFactory;
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var string[]
+     */
+    protected $fillable = [
+        'user_id',
+        'description',
+        'is_login',
+        'ip_address',
+        'browser',
+    ];
+}
