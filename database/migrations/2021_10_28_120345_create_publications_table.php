68cdfce92b77298199207cdd052c79010a1a2b2f
Glenaldin Lim <35095375+glenaldinlim@users.noreply.github.com>
2021-10-28 12:35:10 +0700

    Add Publication Model and Migration

---
+++ database/migrations/2021_10_28_120345_create_publications_table.php
@@ -0,0 +1,43 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreatePublicationsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('publications', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('user_id');
+            $table->enum('type', ['News', 'Article']);
+            $table->string('title');
+            $table->string('slug');
+            $table->longText('content');
+            $table->string('cover');
+            $table->enum('published_status', ['Draft', 'Publish']);
+            $table->timestamp('published_at');
+            $table->foreignId('published_by');
+            $table->timestamps();
+
+            $table->foreign('user_id')->references('id')->on('users');
+            $table->foreign('published_by')->references('id')->on('users');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('publications');
+    }
+}
