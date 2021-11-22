d6ebafe1c20aaf56d9cc62718653bd0caf77cbad
Glenaldin Lim <35095375+glenaldinlim@users.noreply.github.com>
2021-11-22 17:23:43 +0700

    Change Category Menu Icon; Change Languange to Indonesia; Add Two Menu's

--- resources/views/layouts/backend/_navbar.blade.php
+++ resources/views/layouts/backend/_navbar.blade.php
@@ -2,16 +2,20 @@
     <li class="menu-header">Main</li>
     <li class="{{ (Request::segment(2) == 'dashboard') ? 'active' : '' }}"><a class="nav-link" href="{{ route('backend.dashboard') }}"><i class="fas fa-fire"></i>&nbsp;<span>Dashboard</span></a></li>
 
-    <li class="menu-header">Manage HR</li>
-    <li class="{{ (Request::segment(2) == 'users') ? 'active' : '' }}"><a class="nav-link" href="{{ route('backend.users.index') }}"><i class="fas fa-user-lock"></i>&nbsp;<span> Administrator</span></a></li>
+    <li class="menu-header">Kelola Administrator</li>
+    <li class="{{ (Request::segment(2) == 'users') ? 'active' : '' }}"><a class="nav-link" href="{{ route('backend.users.index') }}"><i class="fas fa-user-lock"></i>&nbsp;<span>Pengurus</span></a></li>
 
-    <li class="menu-header">Manage Category</li>
-    <li class="{{ (Request::segment(2) == 'category') ? 'active' : '' }}"><a class="nav-link " href="{{ route('backend.categories.index') }}"><i class="fas fa-columns"></i>&nbsp;<span>Category</span></a></li>
-    <li class="{{ (Request::segment(2) == 'commodities') ? 'active' : '' }}"><a class="nav-link" href="{{ route('backend.commodities.index') }}"><i class="fas fa-seedling"></i>&nbsp;<span>Commodities</span></a></li>
+    <li class="menu-header">Kelola Komoditas</li>
+    <li class="{{ (Request::segment(2) == 'category') ? 'active' : '' }}"><a class="nav-link " href="{{ route('backend.categories.index') }}"><i class="fas fa-tag"></i>&nbsp;<span>Kategori</span></a></li>
+    <li class="{{ (Request::segment(2) == 'commodities') ? 'active' : '' }}"><a class="nav-link" href="{{ route('backend.commodities.index') }}"><i class="fas fa-seedling"></i>&nbsp;<span>Komoditas</span></a></li>
 
-    <li class="menu-header">Manage Community</li>
-    <li class="{{ (Request::segment(2) == 'communities') ? 'active' : '' }}"><a class="nav-link" href="{{ route('backend.communities.index') }}"><i class="fas fa-users"></i>&nbsp;<span> Community</span></a></li>
+    <li class="menu-header">Kelola Rekan</li>
+    <li class="{{ (Request::segment(2) == 'communities') ? 'active' : '' }}"><a class="nav-link" href="{{ route('backend.communities.index') }}"><i class="fas fa-users"></i>&nbsp;<span>Komunitas</span></a></li>
 
-    <li class="menu-header">Manage Publication</li>
-    <li class="{{ (Request::segment(2) == 'publications') ? 'active' : '' }}"><a class="nav-link" href="{{ route('backend.publications.index') }}"><i class="fas fa-bullhorn"></i>&nbsp;<span>Publication</span></a></li>
+    <li class="menu-header">Kelola Publikasi</li>
+    <li class="{{ (Request::segment(2) == 'publications') ? 'active' : '' }}"><a class="nav-link" href="{{ route('backend.publications.index') }}"><i class="fas fa-bullhorn"></i>&nbsp;<span>Publikasi</span></a></li>
+
+    <li class="menu-header">Misc</li>
+    <li class="{{ (Request::segment(3) == 'misc' && Request::segment(2) == 'contacts') ? 'active' : '' }}"><a class="nav-link" href="#"><i class="fas fa-inbox"></i>&nbsp;<span>Pesan Kontak</span></a></li>
+    <li class="{{ (Request::segment(3) == 'misc' && Request::segment(2) == 'settings') ? 'active' : '' }}"><a class="nav-link" href="#"><i class="fas fa-cog"></i>&nbsp;<span>Pengaturan Website</span></a></li>
 </ul>
