711cc3effc40d779e0bcf6b1b84ea3c8290ea4ca
Glenaldin Lim <35095375+glenaldinlim@users.noreply.github.com>
2021-11-22 19:58:20 +0700

    Remove Category Show Page File

--- resources/views/backend/category/show.blade.php
+++
@@ -1,9 +0,0 @@
-@extends('layouts.backend.app')
-
-@section('content')
-    <section class="section">
-      <div class="section-header">
-        <h1>Kategori Komoditas</h1>
-      </div>
-    </section>
-@endsection
