dab3c05bc97d986cf412919ed9a5128ff306c217
Glenaldin Lim <35095375+glenaldinlim@users.noreply.github.com>
2021-11-24 21:17:28 +0700

    Adding Information to Dashboard

--- app/Http/Controllers/Backend/DashboardController.php
+++ app/Http/Controllers/Backend/DashboardController.php
@@ -2,8 +2,14 @@
 
 namespace App\Http\Controllers\Backend;
 
-use App\Http\Controllers\Controller;
+use App\Models\User;
+use App\Models\Contact;
+use App\Models\Category;
+use App\Models\Commodity;
+use App\Models\Community;
+use App\Models\Publication;
 use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
 
 class DashboardController extends Controller
 {
@@ -24,6 +30,27 @@ class DashboardController extends Controller
      */
     public function index()
     {
-        return view('backend.dashboard');
+        $rangeTimeFrom = date('Y-m-d').' 00:00:00';
+        $rangeTimeTo = date('Y-m-d', strtotime('+1 day')).' 00:00:00';
+
+        $users = User::role(['bod', 'webmaster', 'admin'])->count();
+        $categories = Category::count();
+        $commodities = Commodity::count();
+        $communities = Community::count();
+        $publicationsDraft = Publication::where('published_status', 'Draft')->count();
+        $publicationsPublish = Publication::where('published_status', 'Publish')->count();
+        $messagesToday = Contact::whereBetween('created_at', [$rangeTimeFrom, $rangeTimeTo])->count();
+        $messagesAll = Contact::count();
+
+        return view('backend.dashboard', [
+            'users'                 => $users,
+            'categories'            => $categories,
+            'commodities'           => $commodities,
+            'communities'           => $communities,
+            'publicationsDraft'     => $publicationsDraft,
+            'publicationsPublish'   => $publicationsPublish,
+            'messagesToday'         => $messagesToday,
+            'messagesAll'           => $messagesAll,
+        ]);
     }
 }
--- resources/views/backend/dashboard.blade.php
+++ resources/views/backend/dashboard.blade.php
@@ -13,10 +13,115 @@
                     </div>
                     <div class="card-wrap">
                         <div class="card-header">
-                            <h4>Users</h4>
+                            <h4>Pengurus</h4>
                         </div>
                         <div class="card-body">
-                            0
+                            {{ $users }}
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="col-lg-3 col-md-6 col-sm-6 col-12">
+                <div class="card card-statistic-1">
+                    <div class="card-icon bg-info text-white">
+                        <i class="fas fa-tag fa-2x"></i>
+                    </div>
+                    <div class="card-wrap">
+                        <div class="card-header">
+                            <h4>Kategori</h4>
+                        </div>
+                        <div class="card-body">
+                            {{ $categories }}
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="col-lg-3 col-md-6 col-sm-6 col-12">
+                <div class="card card-statistic-1">
+                    <div class="card-icon bg-warning text-white">
+                        <i class="fas fa-seedling fa-2x"></i>
+                    </div>
+                    <div class="card-wrap">
+                        <div class="card-header">
+                            <h4>Komoditas</h4>
+                        </div>
+                        <div class="card-body">
+                            {{ $commodities }}
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="col-lg-3 col-md-6 col-sm-6 col-12">
+                <div class="card card-statistic-1">
+                    <div class="card-icon bg-danger text-white">
+                        <i class="fas fa-users fa-2x"></i>
+                    </div>
+                    <div class="card-wrap">
+                        <div class="card-header">
+                            <h4>Komunitas</h4>
+                        </div>
+                        <div class="card-body">
+                            {{ $communities }}
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="col-lg-3 col-md-6 col-sm-6 col-12">
+                <div class="card card-statistic-1">
+                    <div class="card-icon bg-secondary text-white">
+                        <i class="fas fa-bullhorn fa-2x"></i>
+                    </div>
+                    <div class="card-wrap">
+                        <div class="card-header">
+                            <h4>Publikasi (Draft)</h4>
+                        </div>
+                        <div class="card-body">
+                            {{ $publicationsDraft }}
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="col-lg-3 col-md-6 col-sm-6 col-12">
+                <div class="card card-statistic-1">
+                    <div class="card-icon bg-success text-white">
+                        <i class="fas fa-bullhorn fa-2x"></i>
+                    </div>
+                    <div class="card-wrap">
+                        <div class="card-header">
+                            <h4>Publikasi (Publish)</h4>
+                        </div>
+                        <div class="card-body">
+                            {{ $publicationsPublish }}
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="col-lg-3 col-md-6 col-sm-6 col-12">
+                <div class="card card-statistic-1">
+                    <div class="card-icon bg-info text-white">
+                        <i class="fas fa-inbox fa-2x"></i>
+                    </div>
+                    <div class="card-wrap">
+                        <div class="card-header">
+                            <h4>Pesan Masuk (Hari Ini)</h4>
+                        </div>
+                        <div class="card-body">
+                            {{ $messagesToday }}
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="col-lg-3 col-md-6 col-sm-6 col-12">
+                <div class="card card-statistic-1">
+                    <div class="card-icon bg-warning text-white">
+                        <i class="fas fa-inbox fa-2x"></i>
+                    </div>
+                    <div class="card-wrap">
+                        <div class="card-header">
+                            <h4>Pesan Masuk (Keseluruhan)</h4>
+                        </div>
+                        <div class="card-body">
+                            {{ $messagesAll }}
                         </div>
                     </div>
                 </div>
