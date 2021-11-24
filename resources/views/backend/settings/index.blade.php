e56ae5036c78d1a741a355a9a3a28525a7eea767
Glenaldin Lim <35095375+glenaldinlim@users.noreply.github.com>
2021-11-24 18:39:43 +0700

    Change Website Setting Title Page

--- resources/views/backend/settings/index.blade.php
+++ resources/views/backend/settings/index.blade.php
@@ -3,7 +3,7 @@
 @section('content')
     <section class="section">
         <div class="section-header">
-            <h1>Pengaturan Website</h1>
+            <h1>Kelola Pengaturan Website</h1>
         </div>
         <div class="section-body">
             <div class="row">
